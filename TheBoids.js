5c1ccb3b4f8a9a6124237cc5382931304b637957
Tim Pinkerton <tpinkerton@wisc.edu>
2020-02-20 23:14:27 -0600

    Comment update to match README

--- TheBoids.js
+++ TheBoids.js
@@ -1,5 +1,5 @@
 /**
- * This project originated as a small homework assignment for Michael Gleicher's CS 559 - Graphics class, Spring of 2019; starter code was provided by Michael Gleicher.
+ * This project originated as a small homework assignment for Michael Gleicher's UW-Madison CS 559 - Graphics class, Spring of 2019; starter code was provided by Michael Gleicher.
  * Since then, it has been expanded considerably as a hobby project--(virtually) all of the starter code has been replaced/rewritten.
  * 
  * Written by Tim Pinkerton.
